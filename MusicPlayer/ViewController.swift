fdfae3fd135f4b084a38ae735fc6f7dab60bce4a
Ken Wilcox <wilcoxk@gmail.com>
2018-01-11 21:58:36 -0700

    Trying out a few things

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -8,7 +8,7 @@ import MediaPlayer
 import UIKit
 
 class ViewController: UIViewController, MPMediaPickerControllerDelegate {
-
+    
     @IBOutlet weak var albumImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var artistLabel: UILabel!
@@ -24,25 +24,28 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
     @IBOutlet weak var forwardBackground: UIView!
     @IBOutlet weak var repeatButton: UIButton!
     @IBOutlet weak var shuffleButton: UIButton!
-
+    
     var musicPlayer: MPMusicPlayerController!
-
-    var timer: Timer!
-
+    
+    var timer: Timer?
+    let pauseImage = UIImage(named: "Pause")
+    let playImage = UIImage(named: "Play")
+    var timerCount = 0
+    
     var isPlaying: Bool! {
         didSet {
+            print("isPlaying didSet")
             if isPlaying {
-                if let image = UIImage(named: "Pause") {
-                    playPauseButton.setImage(image, for: .normal)
-                }
+                playPauseButton.setImage(pauseImage, for: .normal)
+                timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateThumb), userInfo: nil, repeats: true)
+                timer?.tolerance = 0
             } else {
-                if let image = UIImage(named: "Play") {
-                    playPauseButton.setImage(image, for: .normal)
-                }
+                playPauseButton.setImage(playImage, for: .normal)
+                timer?.invalidate()
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if #available(iOS 10.3, *) {
@@ -56,18 +59,17 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
         musicPlayer.repeatMode = .default
         musicPlayer.shuffleMode = .default
         isPlaying = musicPlayer.playbackState == .playing
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateInterface), userInfo: nil, repeats: true)
         updateInterface()
         NotificationCenter.default.addObserver(self, selector: #selector(updateInterface), name: NSNotification.Name.MPMusicPlayerControllerNowPlayingItemDidChange, object: nil)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Media Picker Controller Delegate
-
+    
     func mediaPicker(_ mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
         musicPlayer.setQueue(with: mediaItemCollection)
         mediaPicker.dismiss(animated: true) {
@@ -76,20 +78,20 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
         musicPlayer.play()
         isPlaying = true
     }
-
+    
     func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
         mediaPicker.dismiss(animated: true, completion: nil)
     }
-
+    
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-
+    
     // MARK: - Actions
-
+    
     @IBAction func touchedDown(_ sender: UIButton) {
         let buttonBackground: UIView
         switch sender {
@@ -107,7 +109,7 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
             sender.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
         }
     }
-
+    
     @IBAction func touchedUpInside(_ sender: UIButton) {
         let buttonBackground: UIView
         switch sender {
@@ -128,30 +130,31 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
             buttonBackground.transform = CGAffineTransform.identity
         }
     }
-
+    
     @IBAction func backwardButtonTapped(_ sender: UIButton) {
         musicPlayer.skipToPreviousItem()
     }
-
+    
     @IBAction func playPauseButtonTapped(_ sender: UIButton) {
         if isPlaying {
-            musicPlayer.pause()
             UIView.animate(withDuration: 0.5) {
                 self.albumImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
             }
+            musicPlayer.pause()
         } else {
-            musicPlayer.play()
             UIView.animate(withDuration: 0.5) {
                 self.albumImageView.transform = CGAffineTransform.identity
             }
+            musicPlayer.play()
         }
         isPlaying = !isPlaying
+        updateThumb()
     }
-
+    
     @IBAction func forwardButtonTapped(_ sender: UIButton) {
         musicPlayer.skipToNextItem()
     }
-
+    
     @IBAction func repeatButtonTapped(_ sender: UIButton) {
         switch musicPlayer.repeatMode {
         case .all:
@@ -173,7 +176,7 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
             break
         }
     }
-
+    
     @IBAction func musicButtonTapped(_ sender: UIButton) {
         let mediaPicker = MPMediaPickerController(mediaTypes: .music)
         mediaPicker.allowsPickingMultipleItems = true
@@ -182,7 +185,7 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
         mediaPicker.delegate = self
         present(mediaPicker, animated: true, completion: nil)
     }
-
+    
     @IBAction func shuffleButtonTapped(_ sender: UIButton) {
         switch musicPlayer.shuffleMode {
         case .albums:
@@ -204,12 +207,15 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
             break
         }
     }
-
+    
     // MARK: - Helpers
-
+    
+    // This doesn't need to be updated every timer fire
     @objc func updateInterface() {
         guard let playerItem = musicPlayer.nowPlayingItem else { return }
-        guard !musicPlayer.currentPlaybackTime.isNaN else { return }
+        titleLabel.text = playerItem.title
+        artistLabel.text = playerItem.artist ?? playerItem.albumArtist ?? "Unknown Artist"
+        albumLabel.text = playerItem.albumTitle ?? "Unknown Album"
         let orientation = UIApplication.shared.statusBarOrientation
         switch orientation {
         case .portrait:
@@ -219,19 +225,26 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
         default:
             break
         }
-        titleLabel.text = playerItem.title
-        artistLabel.text = playerItem.artist ?? playerItem.albumArtist ?? "Unknown Artist"
-        albumLabel.text = playerItem.albumTitle ?? "Unknown Album"
+        playbackTimeSlider.maximumValue = Float(playerItem.playbackDuration)
+        updateThumb()
+    }
+    
+    // This does need to update every timer fire
+    @objc func updateThumb() {
+        timerCount += 1
+        print("updateThumb \(timerCount)")
+        //guard !musicPlayer.currentPlaybackTime.isNaN else {return}
         let playbackTime = musicPlayer.currentPlaybackTime
         let playbackTimeMinutes = Int(playbackTime / 60)
         let playbackTimeSeconds = Int(playbackTime.truncatingRemainder(dividingBy: 60))
         playbackTimeLabel.text = String(format: "%01d:%02d", arguments: [playbackTimeMinutes, playbackTimeSeconds])
         playbackTimeSlider.value = Float(musicPlayer.currentPlaybackTime)
-        playbackTimeSlider.maximumValue = Float(playerItem.playbackDuration)
+        
+        guard let playerItem = musicPlayer.nowPlayingItem else { return }
         let playbackTimeDelta = playerItem.playbackDuration - musicPlayer.currentPlaybackTime
         let playbackTimeDeltaMinutes = Int(playbackTimeDelta / 60)
         let playbackTimeDeltaSeconds = Int(playbackTimeDelta.truncatingRemainder(dividingBy: 60))
         playbackTimeDeltaLabel.text = String(format: "-%01d:%02d", arguments: [playbackTimeDeltaMinutes, playbackTimeDeltaSeconds])
     }
-
+    
 }
